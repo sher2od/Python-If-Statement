4518c34f238685b085868d85760a2ce2ae536b54
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-03 23:00:04 +0300

    task10

---
+++ task10.py
@@ -0,0 +1,16 @@
+ball = float(input("balingiz >> "))
+
+if 90 < ball < 100:
+    print("A")
+
+if 80 < ball < 89:
+    print("B")
+
+if 70 < ball < 79:
+    print("c")
+
+if 60 < ball < 69:
+    print("D")
+
+if 0 < ball < 59:
+    print("F")
