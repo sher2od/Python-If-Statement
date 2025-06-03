e60b093fd40f74303ca2d2d2d861ab8400072b4f
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-03 22:55:21 +0300

    task09

---
+++ task09.py
@@ -0,0 +1,12 @@
+t_yon1 = float(input())
+t_yon2 = float(input())
+t_asosi = float(input())
+
+if t_yon1 == t_yon2 == t_asosi:
+    print("muntazam")
+
+elif t_yon1 == t_yon2:
+    print("teng yonli")
+
+else:
+    print("turli tomonli")
