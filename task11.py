4aa4405945d71fa43dc56807f854fa9b4666229f
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-03 23:09:41 +0300

    task12

---
+++ task11.py
@@ -0,0 +1,13 @@
+oy = int(input())
+
+if oy == 12 and oy == 1 and oy == 2:
+    print("Qish")
+
+if oy == 3 and oy == 4 and oy == 5:
+    print("Bahor")
+
+if oy == 6 and oy == 7 and oy == 8:
+    print("Yoz")
+
+if oy == 9 and oy == 10 and oy == 11:
+    print("Kuz")
---
+++ task12.py
@@ -0,0 +1,9 @@
+password1 = input()
+password2 = input()
+
+if password1 == password2:
+    print("Parol togri")
+
+
+else:
+    print("afsuski parol xato")
